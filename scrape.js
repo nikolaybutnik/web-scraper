351465ff3ac6e43252b9f580767659f3b0066213
Nikolay Butnik <btnk.nik@gmail.com>
2021-08-31 20:23:38 -0400

    Obtain titles of all available articles

---
+++ scrape.js
@@ -0,0 +1,15 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const base = 'http://jamesqquick.com'
+
+axios.get(`${base}/talks`).then((res) => {
+  const talks = []
+  const $ = cheerio.load(res.data)
+
+  $('.card').each((index, element) => {
+    const title = $(element).children().eq(1).children('h3').text()
+    talks[index] = { title }
+  })
+  console.log(talks)
+})
